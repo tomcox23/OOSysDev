bdc4dc479b78b37224c18c467663e981cccecaef
coxts2 <coxts2@student.op.ac.nz>
2019-03-08 23:31:20 +1300

    will now play however many rounds you wont and display the results. need to work on implementing the actual play styles

--- BlackJack3.py
+++ BlackJack3.py
@@ -2,6 +2,13 @@ import random
 
 BLACKJACK = 21
 DEALERS_RULE = 17
+AGGRESSIVE = 17
+PASSIVE = 16
+
+num_round = 0
+wins = 0
+lose = 0
+draw = 0
 
 
 class Deck(object):
@@ -39,67 +46,87 @@ class Player(object):
     def show_hand(self):
         print(self.name + " cards: " + str(self.hand) + " (total: " + str(self.total) + ")")
 
+    def play_aggressive(self):
+        self.play_aggressive()
+        if player.total <= AGGRESSIVE:
+            player.hit(card_deck)
+
+    def play_passive(self):
+        self.play_passive()
+        if player.total <= PASSIVE:
+            player.hit(card_deck)
+
 
 if __name__ == "__main__":
 
-    card_deck = Deck()
-    card_deck.shuffle()
-    player = Player("Your")
-    house = Player("House")
+    while num_round != 10:
+        num_round += 1
 
-    player.hit(card_deck)
-    house.hit(card_deck)
+        card_deck = Deck()
+        card_deck.shuffle()
+        player = Player("Your")
+        house = Player("House")
 
-    player.show_hand()
-    house.show_hand()
-
-    while player.total <= BLACKJACK:
-        player_input = input("Would you like to (H)it or (S)tand? > ").upper()
-        if player_input == 'H' and player.total < BLACKJACK:
-            player.hit(card_deck)
-            player.show_hand()
-            if player.total > BLACKJACK:
-                print("Busted, You lose!")
-                break
-            elif player.total == BLACKJACK:
-                print("BlackJack, You got 21")
-            continue
-        elif player_input == 'S':
-            print("Stand")
-
-        while house.total < DEALERS_RULE:
-            print("House hits")
-            house.hit(card_deck)
-            house.show_hand()
-            if house.total > BLACKJACK:
-                print("House Busted")
-                break
-            elif house.total >= DEALERS_RULE:
-                print("House stands")
-                break
+        player.hit(card_deck)
+        house.hit(card_deck)
 
         player.show_hand()
         house.show_hand()
 
-        if (player.total > house.total) and (player.total <= BLACKJACK):
-            print("You win!")
+        while player.total <= BLACKJACK:
+            # player_input = input("Would you like to (H)it or (S)tand? > ").upper()
+            # if player_input == 'H' and player.total < BLACKJACK:
+            if player.total < BLACKJACK:
+                player.hit(card_deck)
+                player.show_hand()
+                if player.total > BLACKJACK:
+                    print("Busted, You lose!")
+                    lose += 1
+                    break
+                elif player.total == BLACKJACK:
+                    print("BlackJack, You got 21")
 
-            # without this line below player will lose if house total is more then 21 (bust) and players total is
-            # less then house. this now results in a player win
-            # Your cards: ['3', 'Jack', '4'](total: 17)
-            # House cards: ['3', 'Jack', '9'](total: 22)
-            # House wins
+                continue
+            # elif player_input == 'S':
+            # print("Stand")
 
-        elif(player.total < house.total) and (house.total > BLACKJACK):
-            print("You win!")
+            while house.total < DEALERS_RULE:
+                print("House hits")
+                house.hit(card_deck)
+                house.show_hand()
+                if house.total > BLACKJACK:
+                    print("House Busted")
 
-            # now returns the correct player win
-            # Your cards: ['3', 'Jack', '4'](total: 17)
-            # House cards: ['3', 'Jack', '9'](total: 22)
-            # You wins
+                    break
+                elif house.total >= DEALERS_RULE:
+                    print("House stands")
+                    break
 
-        elif (player.total < house.total) and (house.total <= BLACKJACK):
-            print("House wins!")
-        else:
-            print("Push!")
-        break
+            player.show_hand()
+            house.show_hand()
+
+            if (player.total > house.total) and (player.total <= BLACKJACK):
+                print("You win!")
+                wins += 1
+                # without this line below player will lose if house total is more then 21 (bust) and players total is
+                # less then house. this now results in a player win
+                # Your cards: ['3', 'Jack', '4'](total: 17)
+                # House cards: ['3', 'Jack', '9'](total: 22)
+                # House wins
+
+            elif(player.total < house.total) and (house.total > BLACKJACK):
+                print("You win!")
+                wins += 1
+                # now returns the correct player win
+                # Your cards: ['3', 'Jack', '4'](total: 17)
+                # House cards: ['3', 'Jack', '9'](total: 22)
+                # You win
+
+            elif (player.total < house.total) and (house.total <= BLACKJACK):
+                print("House wins!")
+                lose += 1
+            else:
+                print("Push!")
+                draw += 1
+            break
+        print("W:", wins, "L:", lose, "D:", draw, "R", num_round)
